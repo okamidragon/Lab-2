c30abd1d08df7ed28672d1710d3160517cbf751f
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-18 21:54:47 -0500

    Update Deadline.java

--- Lab 2/src/Deadline.java
+++ Lab 2/src/Deadline.java
@@ -3,29 +3,22 @@ import java.util.Date;
 public class Deadline extends Event implements Completable {
     private boolean complete = false;
 
-    // Constructor
     public Deadline(String name, Date dateTime) {
         super(name, dateTime);
     }
 
     @Override
     public String getName() {
-        return "Deadline: " + getBaseName() + " by " + getDateTime();
+        return name; // Return the name of the deadline
     }
 
-    // Implement Completable interface methods
     @Override
     public void complete() {
-        this.complete = true;
+        this.complete = true; // Set completion status
     }
 
     @Override
     public boolean isComplete() {
-        return this.complete;
-    }
-
-    @Override
-    public String toString() {
-        return getName() + " | Complete: " + isComplete();
+        return this.complete; // Return completion status
     }
 }
