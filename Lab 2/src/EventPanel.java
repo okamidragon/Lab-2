661a7bf8addf3ff5a4efe93d59005e70f9b2a51d
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-20 20:24:14 -0500

    Update EventPanel.java

--- Lab 2/src/EventPanel.java
+++ Lab 2/src/EventPanel.java
@@ -1,15 +1,53 @@
 import javax.swing.*;
 import java.awt.*;
+import java.time.LocalDateTime;
 
 public class EventPanel extends JPanel {
-    private final Event event;
+    private Event event;
+    private JButton completeButton;
 
     public EventPanel(Event event) {
         this.event = event;
-        setLayout(new GridLayout(1, 1)); // Adjust layout as needed
-        JLabel nameLabel = new JLabel(event.getName());
-        JLabel dateLabel = new JLabel(event.getDateTime().toString());
-        add(nameLabel);
-        add(dateLabel);
+        setLayout(new BorderLayout());
+
+        completeButton = new JButton("Complete");
+        completeButton.setVisible(event instanceof Completable);
+
+        // Set up the display
+        updateDisplay();
+        updateUrgency();
+
+        completeButton.addActionListener(e -> {
+            if (event instanceof Completable) {
+                ((Completable) event).complete();
+                updateDisplay();
+            }
+        });
+
+        add(completeButton, BorderLayout.SOUTH);
+    }
+
+    private void updateDisplay() {
+        String displayText = String.format("Name: %s\nDate: %s\nComplete: %s",
+                event.getName(), event.getDateTime(), (event instanceof Completable && ((Completable) event).isComplete()));
+        removeAll();
+        add(new JLabel(displayText), BorderLayout.CENTER);
+        revalidate();
+        repaint();
+    }
+
+    public void updateUrgency() {
+        Urgency urgency = Urgency.getUrgency(event.getDateTime());
+        switch (urgency) {
+            case OVERDUE:
+                setBackground(Color.RED);
+                break;
+            case IMMINENT:
+                setBackground(Color.YELLOW);
+                break;
+            case DISTANT:
+                setBackground(Color.GREEN);
+                break;
+        }
     }
 }
