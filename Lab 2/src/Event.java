f178933954ece3a8c364e7cb771716ccac2239fc
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-24 21:50:33 -0500

    Update Event.java

--- Lab 2/src/Event.java
+++ Lab 2/src/Event.java
@@ -4,25 +4,31 @@ public abstract class Event implements Comparable<Event> {
     protected String name;
     protected LocalDateTime dateTime;
 
+    // Data of an Event
     public Event(String name, LocalDateTime dateTime) {
         this.name = name;
         this.dateTime = dateTime;
     }
 
+    // Name of Event
     public abstract String getName();
 
+    // Location of Event
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
+    // Date of the Event
     public void setDateTime(LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 
+    // Update the name of the event
     public void setName(String name) {
         this.name = name;
     }
 
+    // Compares dates
     public int compareTo(Event e) {
         return this.dateTime.compareTo(e.dateTime);
     }
