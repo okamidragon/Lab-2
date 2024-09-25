e5eb5acde8f33d5f57788c1b906bcba0f7e8d206
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-24 21:50:18 -0500

    Update Deadline.java

--- Lab 2/src/Deadline.java
+++ Lab 2/src/Deadline.java
@@ -3,19 +3,23 @@ import java.time.LocalDateTime;
 public class Deadline extends Event implements Completable {
     private boolean complete;
 
+    // Data for deadline
     public Deadline(String name, LocalDateTime dateTime) {
         super(name, dateTime);
         this.complete = false;
     }
 
+    // Name of deadline
     public String getName() {
         return name;
     }
 
+    // Completes the deadline
     public void complete() {
         complete = true;
     }
 
+    // Checks if completed
     public boolean isComplete() {
         return complete;
     }
