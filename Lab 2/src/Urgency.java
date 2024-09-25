03fc1412db2bbecc61b3d758581a99a3ebae21e9
Maddock <143650411+okamidragon@users.noreply.github.com>
2024-09-24 21:52:31 -0500

    Update Urgency.java

--- Lab 2/src/Urgency.java
+++ Lab 2/src/Urgency.java
@@ -4,12 +4,16 @@ import java.time.LocalDateTime;
 public enum Urgency {
     DISTANT, IMMINENT, OVERDUE;
 
-    private static Duration thresholdOfImminence = Duration.ofDays(1); // Example threshold
+    // Example threshold
+    private static Duration thresholdOfImminence = Duration.ofDays(1);
 
+    // Sets the threshold of imminence
     public static void setThresholdOfImminence(Duration d) {
         thresholdOfImminence = d;
     }
 
+    // Logic to determine how close an events date is to the current date,
+    // Uses that logic to decide the urgency of the event
     public static Urgency getUrgency(LocalDateTime time) {
         LocalDateTime now = LocalDateTime.now();
         if (time.isBefore(now)) {
